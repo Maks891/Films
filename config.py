3e82b62e46d6781753cfd961e3cdd5273208a0f0
Maks891 <124050452+Maks891@users.noreply.github.com>
2024-05-13 20:46:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # group ids or account ids can be retrieved with @username_to_id_bot
-BOT_TOKEN="6209129607:AAGMGfEDYprOGuHMtdOk6VjhMdtSPiTSniI"
-BOT_OWNER=1326911178
+BOT_TOKEN="7092521991:AAG18Ty2cie-czUSvfhQmyWo9sZmIpHtYos"
+BOT_OWNER=6888643375
